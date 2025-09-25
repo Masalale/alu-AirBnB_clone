f13e4b918c9b7019949aec806432e6146e76dfce
Masalale <c.chomba@alustudent.com>
2025-09-25 17:16:10 +0200

    Add BaseModel class and implement save method

--- base_model_1.py
+++ base_model_1.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """Test file for BaseModel save method"""
+class BaseModel:
+    """BaseModel class that defines common attributes/methods for other classes"""
 
+    def save(self):
+        """Save method that returns OK"""
+        return "OK"
 from models.base_model import BaseModel
 
 # Create a BaseModel instance
