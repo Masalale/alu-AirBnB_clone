4d37f0a0a41eea26e8424531f79b5882e44769fe
Masalale <c.chomba@alustudent.com>
2025-09-25 17:50:06 +0200

    Refactor BaseModel class docstring and improve date handling in initialization; streamline to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 
 class BaseModel:
-    """BaseModel class that defines common attributes/methods for other classes"""
+    """Base class for all models in AirBnB clone"""
 
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance"""
@@ -14,8 +14,9 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
-                elif key in ['created_at', 'updated_at']:
-                    setattr(self, key, datetime.fromisoformat(value))
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
                     setattr(self, key, value)
         else:
@@ -34,50 +35,10 @@ class BaseModel:
         self.updated_at = datetime.now()
         import models
         models.storage.save()
+        return "OK"
 
     def to_dict(self):
         """Returns dictionary representation of BaseModel"""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
-import uuid
-from datetime import datetime
-import models
-
-
-class BaseModel:
-    """Base class for all models in AirBnB clone"""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize BaseModel instance"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f"))
-                else:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-
-    def __str__(self):
-        """String representation of BaseModel instance"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
-    def save(self):
-        """Update updated_at with current datetime and save to storage"""
-        self.updated_at = datetime.now()
-        models.storage.save()
-
-    def to_dict(self):
-        """Return dictionary representation of BaseModel instance"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
